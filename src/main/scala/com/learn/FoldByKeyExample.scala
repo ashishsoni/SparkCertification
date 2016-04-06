9025689b491ec632e613070c04bd6638fa026e92
ashishsoni <ashish.usoni@gmail.com>
2016-04-06 19:35:05 -0400

    combineby key

--- src/main/scala/com/learn/FoldByKeyExample.scala
+++ src/main/scala/com/learn/FoldByKeyExample.scala
@@ -1,8 +1,19 @@
 package com.learn
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by spark on 4/5/16.
   */
 object FoldByKeyExample {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("AggregateBy Key").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val data = sc.parallelize(List("Cat","Mouse","Rat","Dog","Spark","rdd"),2)
+    val pairedRdd = data.map(x => (x.length,x))
+    val foldrdd = pairedRdd.foldByKey("")(_ + _).collect()
+    foldrdd.foreach(println)
+
+  }
 
 }
--- src/main/scala/com/learn/FoldExample.scala
+++ src/main/scala/com/learn/FoldExample.scala
@@ -9,8 +9,9 @@ object FoldExample {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("AggregateBy Key").setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val rdd1 = sc.parallelize(List(1 ,2,3,4,5),5)
-    println(rdd1.fold(10)(_ + _))
+//    val rdd1 = sc.parallelize(List(1 ,2,3,4,5),5)
+//    println(rdd1.fold(10)(_ + _))
+    sc.parallelize(1 to 10,5).fold(0){ (acc,element) => println(s"$acc : $element"); acc + 1}
 
   }
 
