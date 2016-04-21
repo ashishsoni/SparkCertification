818d37f6aaae546ace0f5fcd074e5a71a17ffd3a
ashishsoni <ashish.usoni@gmail.com>
2016-04-21 16:52:42 -0400

    combineby key

---
+++ src/main/scala/com/learn/JoinExample.scala
@@ -0,0 +1,31 @@
+package com.learn
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by spark on 4/21/16.
+  */
+object JoinExample {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("AggregateBy Key").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val rdd1 = sc.parallelize(Seq(
+      ("math",    55),
+
+      ("english", 57),
+
+      ("science", 59)
+      ))
+    val rdd2= sc.parallelize(Seq(
+
+      ("math",    56),
+      ("math",    100),
+
+      ("english", 58),
+
+      ("science", 54)))
+    rdd1.join(rdd2).collect().foreach(println)
+  }
+
+
+}
--- src/main/scala/com/learn/SortByKeyExample.scala
+++ src/main/scala/com/learn/SortByKeyExample.scala
@@ -1,8 +1,23 @@
 package com.learn
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by spark on 4/21/16.
   */
 object SortByKeyExample {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("AggregateBy Key").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val rdd = sc.parallelize(Seq(
+                      ("math",    55),
+                      ("math",    56),
+                      ("english", 57),
+                      ("english", 58),
+                      ("science", 59),
+                      ("science", 54)))
+    val sortedrdd = rdd.sortByKey(true,2)
+    sortedrdd.filterByRange("e","s").foreach(println)
+  }
 
 }
