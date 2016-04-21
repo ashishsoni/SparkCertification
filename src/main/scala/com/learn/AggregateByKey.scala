89b439837b999088be4c30c4236dd1c20ddc8504
ashishsoni <ashish.usoni@gmail.com>
2016-04-21 16:13:22 -0400

    combineby key

--- src/main/scala/com/learn/AggregateByKey.scala
+++ src/main/scala/com/learn/AggregateByKey.scala
@@ -14,7 +14,19 @@ object AggregateByKey {
     val sc = new SparkContext(conf)
     val data = sc.parallelize(Array(("a", 3), ("a", 1), ("b", 7), ("a", 5)))
     val sets = data.aggregateByKey(new HashSet[Int])(_+_,_++_)
-    sets.collect.foreach(println)
+//    sets.collect.foreach(println)
+
+    //Aggregate by Key with Log message
+    val dataRDD = sc.parallelize(List(("a",1),("a",1),("b",1),("b",1)),3)
+    dataRDD.aggregateByKey(0)((acc,value) =>{
+      println("Accum: "+ acc)
+      println("Value: "+ value)
+      acc+value }, (part1result,part2result) => {
+      println("part1result: " + part1result)
+      println("part2result: " + part2result)
+      part1result+part2result
+    }).foreach(println)
+
 
 
   }
---
+++ src/main/scala/com/learn/SortByKeyExample.scala
@@ -0,0 +1,8 @@
+package com.learn
+
+/**
+  * Created by spark on 4/21/16.
+  */
+object SortByKeyExample {
+
+}
